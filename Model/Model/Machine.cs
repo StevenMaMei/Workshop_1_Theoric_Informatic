fc5aad2a96944f608ea45ba0ce90e054077ad076
StevenMaMei <33093117+StevenMaMei@users.noreply.github.com>
2019-03-07 17:28:12 -0500

    created

---
+++ Model/Model/Machine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Machine
+    {
+        Dictionary<char, State> states;
+
+        public Machine()
+        {
+            States = new Dictionary<char, State>();
+        }
+        internal Dictionary<char, State> States { get => states; set => states = value; }
+
+        public void removeInaccesibleStates()
+        {
+            foreach (var a in states.Keys.ToList())
+            {
+
+            }
+        }
+    }
+}
---
+++ Model/Model/State.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class State
+    {
+        char name;
+        Boolean isInitial;
+        Dictionary<char, char> nextStateFunction;
+        Dictionary<char, char> outPutFunction;
+        bool visited;
+
+        public bool IsInitial { get => isInitial; set => isInitial = value; }
+        public Dictionary<char, char> OutPutFunction { get => outPutFunction; set => outPutFunction = value; }
+        public Dictionary<char, char> NextStateFunction { get => nextStateFunction; set => nextStateFunction = value; }
+        public char Name { get => name; set => name = value; }
+        public bool Visited { get => visited; set => visited = value; }
+
+        public State(char n, bool isInit,List<char> inputs, List<char> outputs, List<char> states)
+        {
+            name = n;
+            IsInitial = isInit;
+            NextStateFunction = new Dictionary<char, char>();
+            OutPutFunction = new Dictionary<char, char>();
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                NextStateFunction.Add(inputs[i], states[i]);
+                OutPutFunction.Add(inputs[i], outputs[i]);
+            }
+
+        }
+
+
+
+    }
+}
